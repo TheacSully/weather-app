debbd8572dd0c95fa4fa26af2613d3631908b768
Thea <theacsullivan@gmail.com>
2023-04-24 18:55:28 +0100

    Added updated time function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
+  let dateElement = document.querySelector("#date");
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let currentTime = new Date();
+  dateElement.innerHTML = formatDate(currentTime);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
 }
+
+//let dateElement = document.querySelector("#date");
+//let currentTime = new Date();
+//dateElement.innerHTML = formatDate(currentTime);
 
 let apiKey = "ct90a4732c5fd752o670f2a66b23ca38";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`;
